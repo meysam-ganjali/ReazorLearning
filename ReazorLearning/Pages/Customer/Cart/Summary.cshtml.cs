9a01e220858874672450dadfa745daf9dd1ae544
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-11-04 05:32:31 -0700

    Add Summary UI Page

---
+++ ReazorLearning/Pages/Customer/Cart/Summary.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ReazorLearning.Pages.Customer.Cart
+{
+    public class SummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
