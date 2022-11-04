889aa8f19042212f3c2ee0068adf24c0b5dcce64
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-11-04 16:52:06 -0700

    Post Handler For OrderHeader

--- ReazorLearning/Pages/Customer/Cart/Summary.cshtml.cs
+++ ReazorLearning/Pages/Customer/Cart/Summary.cshtml.cs
@@ -4,10 +4,12 @@ using ReazorLearning.DataLayer.Repository.IRepository;
 using ReazorLearninig.Models.Models;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
+using ReazorLearning.Utilities;
 
 namespace ReazorLearning.Pages.Customer.Cart
 {
     [Authorize]
+    [BindProperties]
     public class SummaryModel : PageModel
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -19,7 +21,8 @@ namespace ReazorLearning.Pages.Customer.Cart
         }
 
         public IEnumerable<ShoppingCart> ShoppingCarts { get; set; }
-        public OrderHeader  OrderHeader{ get; set; }
+        public OrderHeader OrderHeader { get; set; }
+
         public void OnGet()
         {
             var claimIdentity = (ClaimsIdentity)User.Identity;
@@ -41,5 +44,48 @@ namespace ReazorLearning.Pages.Customer.Cart
 
         }
 
+        public IActionResult OnPost()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            if (claim != null)
+            {
+                ShoppingCarts = _unitOfWork.ShoppingCart.GetAll(filter: u => u.ApplicationUserId == claim.Value,
+                    includeProperties: "MenuItem,MenuItem.FoodType,MenuItem.Category");
+
+                foreach (var cartItem in ShoppingCarts)
+                {
+                    OrderHeader.OrderTotal += (cartItem.MenuItem.Price * cartItem.Count);
+                }
+
+                OrderHeader.Status = SD.StatusPending;
+                OrderHeader.OrderDate = System.DateTime.Now;
+                OrderHeader.UserId = claim.Value;
+                OrderHeader.PickUpTime = Convert.ToDateTime(OrderHeader.PickUpDate.ToShortDateString() + " " +
+                                                            OrderHeader.PickUpTime.ToShortTimeString());
+                _unitOfWork.OrderHeader.Add(OrderHeader);
+                _unitOfWork.Save();
+
+                foreach (var item in ShoppingCarts)
+                {
+                    OrderDetail orderDetails = new()
+                    {
+                        MenuItemId = item.MenuItemId,
+                        OrderId = OrderHeader.Id,
+                        Name = item.MenuItem.Name,
+                        Price = item.MenuItem.Price,
+                        Count = item.Count
+                    };
+                    _unitOfWork.OrderDetail.Add(orderDetails);
+
+                }
+
+                _unitOfWork.ShoppingCart.RemoveRange(ShoppingCarts);
+                _unitOfWork.Save();
+                return RedirectToPage("/Customer/Home/Index");
+
+            }
+            return Page();
+        }
     }
 }
