c12200bc03ba45496ae191911b75e97dc8175386
meysam-ganjali <meysam.ghanjali1365@hotmail.com>
2022-11-04 05:23:08 -0700

    Handler Plus.Minus Item And Remove Item From Shopping Cart

--- ReazorLearning/Pages/Customer/Cart/Index.cshtml.cs
+++ ReazorLearning/Pages/Customer/Cart/Index.cshtml.cs
@@ -26,13 +26,43 @@ namespace ReazorLearning.Pages.Customer.Cart
             if (claim != null)
             {
                 ShoppingCarts = _unitOfWork.ShoppingCart.GetAll(filter: u => u.ApplicationUserId.Equals(claim.Value),
-                    includeProperties: "MenuItem.FoodType,MenuItem.Category");
+                    includeProperties: "MenuItem,MenuItem.FoodType,MenuItem.Category");
             }
 
             foreach (var price in ShoppingCarts)
             {
-                TotalPrice += price.MenuItem.Price;
+                TotalPrice += (price.MenuItem.Price * price.Count);
             }
+        }
+
+        public IActionResult OnPostPlus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(filter: c => c.Id==cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            TempData["Msg"] = "Plus Item In Cart";
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+        public IActionResult OnPostMinus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(filter: c => c.Id.Equals(cartId));
+            _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            TempData["Msg"] = "Minus Item In Cart";
+            if (cart.Count == 0)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+                _unitOfWork.Save();
+                TempData["Msg"] = "Remove Item In Cart";
+                return RedirectToPage("/Customer/Cart/Index");
+            }
+            return RedirectToPage("/Customer/Cart/Index");
+        }
+        public IActionResult OnPostRemove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(filter: c => c.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            TempData["Msg"] = "Remove Item In Cart";
+            return RedirectToPage("/Customer/Cart/Index");
         }
     }
 }
